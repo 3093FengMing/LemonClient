2c36eee3de46bfe213440d88f455fdb6143d69c8
Fin <128983509+FinLemonKe@users.noreply.github.com>
2023-05-03 08:28:10 +0800

    Add files via upload

---
+++ src/main/java/meteordevelopment/meteorclient/addons/GithubRepo.java
@@ -0,0 +1,11 @@
+package meteordevelopment.meteorclient.addons;
+
+public record GithubRepo(String owner, String name, String branch) {
+    public GithubRepo(String owner, String name) {
+        this(owner, name, "master");
+    }
+
+    public String getOwnerName() {
+        return owner + "/" + name;
+    }
+}
